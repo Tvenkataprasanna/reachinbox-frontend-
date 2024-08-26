395b2f28361f081bc234cc4005fc7eb0401c715e
venkata prasanna <130022308+Tvenkataprasanna@users.noreply.github.com>
2024-08-26 11:38:27 +0530

    init

---
+++ src/pages/Login.tsx
@@ -0,0 +1,9 @@
+import {Link, useNavigate} from "react-router-dom";
+import AppBar from "../components/AppBar";
+import google from "../assets/google.svg";
+import Footer from "../components/Footer";
+
+function Login(){
+    const Navigate = useNavigate()
+    const token = localStorage.getItem('token')
+}
